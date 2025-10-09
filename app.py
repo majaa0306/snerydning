78c85dac8eb56fcb1390da0e3e63f8bc7b598cd9
Maja0306 <Majand01@iba.dk>
2025-10-09 12:16:49 +0200

    3 forsøg

--- app.py
+++ app.py
@@ -3,20 +3,17 @@ import streamlit as st
 import folium
 from streamlit_folium import st_folium
 from folium.plugins import LocateControl
-from folium import MacroElement
 from jinja2 import Template
 
 # Brug hele siden
 st.set_page_config(layout="wide")
 
-# Google Sheet ID
+# --- Hent data ---
 SHEET_ID = "1DNHbwKxJ9_HKLtfJ_hC0jeHnKlmana_thEBQfr2sMfM"
 url = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=csv"
-
-# Læs data direkte fra Google Sheets
 data = pd.read_csv(url)
 
-# Hvis lat/lon bruger komma som decimal, konverter til punktum
+# Konverter lat/lon (hvis komma som decimal)
 data['lat'] = data['lat'].astype(str).str.replace(',', '.').astype(float)
 data['lon'] = data['lon'].astype(str).str.replace(',', '.').astype(float)
 
@@ -33,44 +30,39 @@ if gade_valg != "Alle":
     data = data[data['adresse'].str.startswith(gade_valg)]
 
 # --- Opret Folium-kort ---
-ansager_lat = 55.703423
-ansager_lon = 8.755025
-# --- Opret kortet (uden cache, så det kan bruge JS-klassen korrekt) ---
-m = folium.Map(location=[55.703423, 8.755025], zoom_start=15)
-LocateControl(auto_start=False).add_to(m)
-m.get_root().add_child(LocateOnLoad())
-
-
+ansager_lat, ansager_lon = 55.703423, 8.755025
+m = folium.Map(location=[ansager_lat, ansager_lon], zoom_start=15)
 
 # Tilføj "Find mig"-knap
 LocateControl(auto_start=False).add_to(m)
 
-# --- Automatisk centrering på brugerens position ---
-class LocateOnLoad(MacroElement):
-    def __init__(self):
-        super().__init__()
-        self._template = Template("""
-            {% macro script(this, kwargs) %}
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(pos) {
-                    var lat = pos.coords.latitude;
-                    var lon = pos.coords.longitude;
-                    {{this._parent.get_name()}}.setView([lat, lon], 17);
-                    L.marker([lat, lon]).addTo({{this._parent.get_name()}})
-                        .bindPopup("📍 Du er her!").openPopup();
-                });
-            }
-            {% endmacro %}
-        """)
+# --- Indsæt JavaScript direkte for flydende live-position ---
+js_code = Template("""
+<script>
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(function(pos) {
+        var lat = pos.coords.latitude;
+        var lon = pos.coords.longitude;
+        if (!window.userMarker) {
+            window.userMarker = L.marker([lat, lon]).addTo({{map_name}});
+            window.userMarker.bindPopup("📍 Du er her!").openPopup();
+            {{map_name}}.setView([lat, lon], 17);
+        } else {
+            window.userMarker.setLatLng([lat, lon]);
+        }
+    },
+    function(err){ console.log(err); },
+    { enableHighAccuracy: true, maximumAge: 5000, timeout: 10000 });
+}
+</script>
+""").render(map_name=m.get_name())
 
-# Tilføj automatisk positionering
-m.get_root().add_child(LocateOnLoad())
+m.get_root().html.add_child(folium.Element(js_code))
 
-# --- Tilføj prikker for adresser ---
+# --- Tilføj prikker ---
 for _, row in data.iterrows():
     farve = 'green' if row['betalt'] == 1 else 'red'
-    
-    # Selve prikken
+
     folium.CircleMarker(
         location=[row['lat'], row['lon']],
         radius=9,
@@ -79,12 +71,10 @@ for _, row in data.iterrows():
         fill_color=farve,
         fill_opacity=0.4
     ).add_to(m)
-    
-    # Træk husnummer ud som sidste ord før komma
-    adresse = row['adresse']
-    husnummer = adresse.split(',')[0].split()[-1]  # sidste ord i første del af adressen
 
-    # Tekst ved siden af prikken
+    adresse = row['adresse']
+    husnummer = adresse.split(',')[0].split()[-1]
+
     folium.map.Marker(
         [row['lat'], row['lon']],
         icon=folium.DivIcon(
@@ -92,5 +82,5 @@ for _, row in data.iterrows():
         )
     ).add_to(m)
 
-# --- Vis kort i fuld bredde ---
+# --- Vis kort ---
 st_folium(m, width=None, height=800)
